3ba7032fb7e5798c903d74173d00f1a6f45e7b61
vans-id <djevann11@gmail.com>
2022-12-12 08:34:42 +0700

    added build push script

---
+++ build_push_image_karsajobs_ui.sh
@@ -0,0 +1,8 @@
+# membuat Image dari Dockerfile
+docker build -t ghcr.io/vans-id/karsajobs-ui:latest .
+
+# Login Github Packages
+echo $GITHUB_TOKEN | docker login ghcr.io -u vans-id --password-stdin
+
+# Push Image ke Github Packages
+docker push ghcr.io/vans-id/item-app:v1
